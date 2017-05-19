ca13e2574965714d725f9a475e3612918353afae
Manish Gadiya <Manish Gadiya>
2017-05-19 16:26:57 -0400

    title

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,8 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'pm-app',
-    template: `
-        <h1>Angular2: Getting Started</h1>
-    `
-})
-export class AppComponent { }
+ import {Component} from '@angular/core';
+ 
+ @Component({
+    selector : 'pm-app',
+ })
+ export class AppComponent {
+    pageTitle : string ="ACME Product Management";
+ }
