6778821c007e351626d78804923ad4c5f47a1c83
Manish Gadiya <Manish Gadiya>
2017-05-22 16:16:10 -0400

    product list component

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,9 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
 
+import {ProductListComponent} from './products/product-list.component'
+
+import {FormsModule} from '@angular/forms';
+
 @NgModule({
-  imports: [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports: [ BrowserModule,FormsModule ],
+  declarations: [ AppComponent,ProductListComponent ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
